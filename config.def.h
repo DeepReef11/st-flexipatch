98f61a2459111c40c6cbad2b84ba969838cf3915
jo <jonathan_92222@hotmail.com>
2024-04-08 13:11:00 -0400

    font2

--- config.def.h
+++ config.def.h
@@ -5,13 +5,15 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "FiraCode Nerd Font:pixelsize=20:antialias=true:autohint=true";
+// static char *font = "FiraCode Nerd Font:pixelsize=20:antialias=true:autohint=true";
+static char *font = "monospace:pixelsize=16"; 
 #if FONT2_PATCH
+static char *font2[] = { "Symbola:pixelsize=12:antialias=true:autohint=true", };
 /* Spare fonts */
-static char *font2[] = {
+// static char *font2[] = {
 /*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
 /*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
-};
+/* } */;
 #endif // FONT2_PATCH
 
 #if BACKGROUND_IMAGE_PATCH
